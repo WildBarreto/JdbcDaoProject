03890d252a7bd463580c77868eaa7daa74dc5f3d
Wild Barreto <44129416+WildBarreto@users.noreply.github.com>
2025-09-07 17:07:36 -0300

    Insert implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -6,6 +6,7 @@ import dao.SellerDao;
 import model.Department;
 import model.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -36,6 +37,12 @@ public class Program {
         for (Seller obj : list){
             System.out.println(obj);
         }
+
+
+        System.out.println("=== TEST 4: seller findAll ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 
 }
